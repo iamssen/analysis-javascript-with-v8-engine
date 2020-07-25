c5a7e2dbf505b94c837a9e33abebb542bb05e4ff
SSen <i@ssen.name>
2020-07-25 19:58:09 +0900

    feat: use console.assert()

--- this/1.global.js
+++ this/1.global.js
@@ -1,20 +1,20 @@
+console.assert(this === globalThis, 'if "this" is on the main. that should be the global object.');
 console.log('>>> main');
-console.log('this is the global object');
 %DebugPrint(this);
 console.log('<<<');
 
-console.log('>>> function');
-console.log('this is the global object');
 function func() {
+  console.assert(this === globalThis, 'if "this" is inside a function. that should be the global object.');
+  console.log('>>> function');
   %DebugPrint(this);
+  console.log('<<<');
 }
 func();
-console.log('<<<');
 
 console.log('>>> arrow');
-console.log('this is the global object');
 const arrow = () => {
+  console.assert(this === globalThis, 'if "this" is inside a arrow function. that should be the global object.');
   %DebugPrint(this);
+  console.log('<<<');
 }
 func();
-console.log('<<<');
--- this/2.global-with-strict.js
+++ this/2.global-with-strict.js
@@ -1,22 +1,22 @@
 'use strict'
 
 console.log('>>> main');
-console.log('this is the global object');
+console.assert(this === globalThis, 'if "this" is on the main and using "use strict". that should be the global object.');
 %DebugPrint(this);
 console.log('<<<');
 
-console.log('>>> function');
-console.log('this is undefined');
 function func() {
+  console.assert(this === undefined, 'if "this" is inside a function and using "use strict". that should be undefined.');
+  console.log('>>> function');
   %DebugPrint(this);
+  console.log('<<<');
 }
 func();
-console.log('<<<');
 
 console.log('>>> arrow');
-console.log('this is the global object');
 const arrow = () => {
+  console.assert(this === globalThis, 'if "this" is inside a arrow function and using "use strict". that should be the global object.');
   %DebugPrint(this);
+  console.log('<<<');
 }
-arrow();
-console.log('<<<');
+func();
